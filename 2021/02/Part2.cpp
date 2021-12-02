d5c86fef9be2999d0a6abcc74930db9d30177bcc
James Wilson <gingersemaj1892@gmail.com>
2021-12-02 09:38:07 +0000

    2021.2: Completed in C++ without tests

---
+++ 2021/02/Part2.cpp
@@ -0,0 +1,34 @@
+#include <string>
+#include <vector>
+
+#include "lib.h"
+
+int main() {
+    std::vector<std::string> lines = readLines("input.txt");
+
+    int depth = 0;
+    int horpos = 0;
+    int aim = 0;
+
+    for (std::string line: lines) {
+        std::vector<std::string> splitLine = split(line, " ");
+        if (splitLine.size() != 2) {
+            puts("Incorrect size");
+            return -1;
+        }
+        std::string dir = splitLine.at(0);
+        int x = std::stoi(splitLine.at(1));
+        if (dir.compare("forward") == 0) {
+            horpos += x;
+            depth += aim * x;
+        } else if (dir.compare("down") == 0) {
+            aim += x;
+        } else if (dir.compare("up") == 0) {
+            aim -= x;
+        }
+    }
+
+    printf("%d\n", depth * horpos);
+
+    return 0;
+}
