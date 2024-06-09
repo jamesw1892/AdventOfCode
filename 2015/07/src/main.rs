31679a66b01feb2a710a44cb00c4557f2bc9ac98
jamesw1892 <gingersemaj1892@gmail.com>
2024-06-09 17:52:05 +0100

    2015-07-01: Make more readable and format

--- 2015/07/src/main.rs
+++ 2015/07/src/main.rs
@@ -1,52 +1,63 @@
+use regex::{Captures, Regex, RegexSet};
+use std::collections::{HashMap, VecDeque};
 use std::env;
 use std::fs;
 use std::io;
 use std::io::BufRead;
 use std::str;
-use std::collections::{HashMap, VecDeque};
-use regex::{Captures, Regex, RegexSet};
 
-fn get_val(text: &str, vals: &mut HashMap<String, u16>, queue: &mut VecDeque<String>, line: &String) -> Option<u16> {
-    text.parse::<u16>().ok().or_else(|| vals.get(text).copied()).or_else(|| {
-        queue.push_back(line.clone());
-        None
-    })
+fn get_val(text: &str, vals: &mut HashMap<String, u16>) -> Option<u16> {
+    text.parse::<u16>().ok().or_else(|| vals.get(text).copied())
 }
 
-fn process_line_part1(regex_set: &RegexSet, regexes: &Vec<Regex>, vals: &mut HashMap<String, u16>, queue: &mut VecDeque<String>, line: String) {
+fn process_line_part1(
+    regex_set: &RegexSet,
+    regexes: &Vec<Regex>,
+    vals: &mut HashMap<String, u16>,
+    queue: &mut VecDeque<String>,
+    line: String,
+) {
     let mut set_matches_iter = regex_set.matches(line.as_str()).into_iter();
     let match_index: usize = set_matches_iter.next().unwrap();
     assert!(set_matches_iter.next().is_none(), "More matches!");
-    let captures: Captures = regexes.get(match_index).unwrap().captures(line.as_str()).unwrap();
+    let captures: Captures = regexes
+        .get(match_index)
+        .unwrap()
+        .captures(line.as_str())
+        .unwrap();
     let output: String = captures.name("output").unwrap().as_str().to_owned();
 
     // Set value
     if match_index == 0 {
-        get_val(captures.name("val").unwrap().as_str(), vals, queue, &line)
-            .and_then(|val: u16| vals.insert(output, val));
+        match get_val(captures.name("val").unwrap().as_str(), vals) {
+            Some(val) => drop(vals.insert(output, val)),
+            None => queue.push_back(line),
+        }
 
     // NOT
     } else if match_index == 1 {
-        get_val(captures.name("input").unwrap().as_str(), vals, queue, &line)
-            .and_then(|input: u16| vals.insert(output, !input));
-
+        match get_val(captures.name("input").unwrap().as_str(), vals) {
+            Some(input) => drop(vals.insert(output, !input)),
+            None => queue.push_back(line),
+        }
     } else {
-        get_val(captures.name("input1").unwrap().as_str(), vals, queue, &line)
-            .and_then(|input1: u16| get_val(captures.name("input2").unwrap().as_str(), vals, queue, &line).map(|input2| (input1, input2)))
-            .and_then(|(input1, input2)| {
-                match match_index {
+        match get_val(captures.name("input1").unwrap().as_str(), vals) {
+            None => queue.push_back(line),
+            Some(input1) => match get_val(captures.name("input2").unwrap().as_str(), vals) {
+                None => queue.push_back(line),
+                Some(input2) => match match_index {
                     // AND
-                    2 => vals.insert(output, input1 & input2),
+                    2 => drop(vals.insert(output, input1 & input2)),
                     // OR
-                    3 => vals.insert(output, input1 | input2),
+                    3 => drop(vals.insert(output, input1 | input2)),
                     // LSHIFT
-                    4 => vals.insert(output, input1 << input2),
+                    4 => drop(vals.insert(output, input1 << input2)),
                     // RSHIFT
-                    5 => vals.insert(output, input1 >> input2),
+                    5 => drop(vals.insert(output, input1 >> input2)),
                     _ => panic!("Impossible: Matched regex out of range"),
-                };
-                Some("hi")
-            });
+                },
+            },
+        }
     }
 }
 
@@ -60,7 +71,10 @@ fn run_part1(filename: &str) {
         r"^(?P<input1>\w+) RSHIFT (?P<input2>\w+) -> (?P<output>\w+)$",
     ];
     let regex_set: RegexSet = RegexSet::new(patterns).unwrap();
-    let regexes: Vec<Regex> = patterns.iter().map(|pattern: &&str| Regex::new(pattern).unwrap()).collect();
+    let regexes: Vec<Regex> = patterns
+        .iter()
+        .map(|pattern: &&str| Regex::new(pattern).unwrap())
+        .collect();
 
     let mut vals: HashMap<String, u16> = HashMap::new();
     let mut queue: VecDeque<String> = VecDeque::new();
@@ -68,7 +82,9 @@ fn run_part1(filename: &str) {
     io::BufReader::new(fs::File::open(filename).unwrap())
         .lines()
         .flatten()
-        .for_each(|line: String| process_line_part1(&regex_set, &regexes, &mut vals, &mut queue, line));
+        .for_each(|line: String| {
+            process_line_part1(&regex_set, &regexes, &mut vals, &mut queue, line)
+        });
 
     while !queue.is_empty() {
         let line = queue.pop_front().unwrap();
