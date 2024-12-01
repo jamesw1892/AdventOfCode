b00b0529f277006100cef2a744f4b11003c6e518
jamesw1892 <gingersemaj1892@gmail.com>
2024-12-01 11:37:03 +0000

    2024-01: Format

--- 2024/01/src/main.rs
+++ 2024/01/src/main.rs
@@ -14,13 +14,23 @@ fn run_part1(filename: &str) {
     let mut left_list: Vec<usize> = Vec::new();
     let mut right_list: Vec<usize> = Vec::new();
     for line in file_lines {
-        let nums: Vec<usize> = line.split("   ").map(|s: &str| s.parse().expect(format!("Failed to parse '{}' to usize", s).as_str())).collect();
+        let nums: Vec<usize> = line
+            .split("   ")
+            .map(|s: &str| {
+                s.parse()
+                    .expect(format!("Failed to parse '{}' to usize", s).as_str())
+            })
+            .collect();
         left_list.push(nums[0]);
         right_list.push(nums[1]);
     }
     left_list.sort();
     right_list.sort();
-    let total_distance: usize = left_list.into_iter().zip(right_list).map(|(n1, n2)| n1.abs_diff(n2)).sum();
+    let total_distance: usize = left_list
+        .into_iter()
+        .zip(right_list)
+        .map(|(n1, n2)| n1.abs_diff(n2))
+        .sum();
     println!("{}", total_distance);
 }
 
@@ -30,11 +40,20 @@ fn run_part2(filename: &str) {
     let mut left_list: Vec<usize> = Vec::new();
     let mut right_list: HashBag<usize> = HashBag::new();
     for line in file_lines {
-        let nums: Vec<usize> = line.split("   ").map(|s: &str| s.parse().expect(format!("Failed to parse '{}' to usize", s).as_str())).collect();
+        let nums: Vec<usize> = line
+            .split("   ")
+            .map(|s: &str| {
+                s.parse()
+                    .expect(format!("Failed to parse '{}' to usize", s).as_str())
+            })
+            .collect();
         left_list.push(nums[0]);
         right_list.insert(nums[1]);
     }
-    let similarity_score: usize = left_list.into_iter().map(|n1: usize| n1 * right_list.contains(&n1)).sum();
+    let similarity_score: usize = left_list
+        .into_iter()
+        .map(|n1: usize| n1 * right_list.contains(&n1))
+        .sum();
     println!("{}", similarity_score);
 }
 
